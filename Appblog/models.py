9bbeada710ce15acbe974930f7da89427acf64aa
JuanCollado00 <144730996+JuanCollado00@users.noreply.github.com>
2023-12-12 12:30:22 -0600

    forms

--- Appblog/models.py
+++ Appblog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+    title=models.CharField(max_length=250)
+    content=models.TextField()
+
+    def __str__(self):
+        return self.title,self.content
+
+class secondpost(models.Model):
+    ejemplo=models.TextField()    
+    
+
+class tercero(models.Model):
+    title3=models.CharField(max_length=250)
+    content3=models.TextField()
+
+    def __str__(self):
+        return self.title3,self.content3
+    
+    
