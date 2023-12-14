bd09993218c60898759f0151544d4e3302fb8cd1
JuanCollado00 <144730996+JuanCollado00@users.noreply.github.com>
2023-12-13 18:17:14 -0600

    mostrar el detail

--- Appblog/views.py
+++ Appblog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
-from .forms import PostCreatForm
+from .forms import PostCreateForm
+from .models import post
 
 # Create your views here.
 class BlogListView(View):
@@ -10,14 +11,30 @@ class BlogListView(View):
         }
         return render(request, 'blog_list.html', context)
     
-
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
+        form=PostCreateForm()
         context={
+            'form':form
         }
-        return render(request,'blog_create.html', context)
+        return render(request, 'blog_create.html', context)
     
     def post(self, request, *args, **kwargs):
-        context={      
+         if request.method=="POST":
+             form = PostCreateForm(request.POST)
+             if form.is_valid():
+                 title = form.cleaned_data.get('title')
+                 content = form.cleaned_data.get('content')
+
+                 p, created = post.objects.get_or_create(title=title, content=content)
+                 p.save()
+                 return redirect('blog:home')
+
+
+
+
+
+         context={
+              
         }
-        return render(request,'blog_create.html', context)
+         return render(request, 'blog_create.html', context)    
